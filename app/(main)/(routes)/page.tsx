c027b604ef31fc9a196413d1194098b35e0f1666
hieunmh <hieunm.pt@gmail.com>
2024-02-17 23:49:59 +0700

    dark light mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,5 @@
+
+import { ModeToggle } from "@/components/modeToggle";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
@@ -5,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
