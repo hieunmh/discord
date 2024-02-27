22677688205de4dbcb5a09a306737641ee65cddc
hieunmh <hieunm.pt@gmail.com>
2024-02-28 01:27:58 +0700

    message attach file

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormField, FormControl, FormItem } from "@/components/ui/form";
 import { Input } from "../ui/input";
 import { Plus, Smile } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -22,6 +23,8 @@ const formSchema = z.object({
 });
 
 export const ChatInput = ({ apiUrl, query, name, type } : ChatInputProps) => {
+
+  const { onOpen } = useModal();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -53,7 +56,7 @@ export const ChatInput = ({ apiUrl, query, name, type } : ChatInputProps) => {
           <FormItem>
             <FormControl>
               <div className="relative p-4 pb-6">
-                <button type="button" onClick={() => {}}
+                <button type="button" onClick={() => onOpen("messageFile", { apiUrl, query })}
                   className=" absolute top-7 left-8 h-[21px] w-[21px] bg-zinc-500 
                   dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 
                   transition rounded-full p-1 flex items-center justify-center"
